e28a1cdf5853c4039b4dd4c50bff191fa6825db5
Stefan Junker <mail@stefanjunker.de>
2021-08-25 18:36:18 +0200

    fixup! release-automation: experiment with cargo fix library

--- crates/release-automation/examples/fix-json.rs
+++ crates/release-automation/examples/fix-json.rs
@@ -2,7 +2,9 @@
 
 #![allow(unused_imports)]
 
-use anyhow::Error;
+use anyhow::{Context, Error};
+use std::fs::File;
+use std::io::Write;
 use std::io::{stdin, BufReader, Read};
 use std::path::PathBuf;
 use std::{collections::HashMap, collections::HashSet, env, fs};
@@ -16,137 +18,163 @@ mod types {
     #[serde(tag = "reason")]
     #[serde(rename = "kebab-case")]
     pub struct CompilerMessage {
-        // pub package_id: String,
-        // pub manifest_path: String,
-        // pub target: Target,
         pub message: rustfix::diagnostics::Diagnostic,
     }
+}
 
-    // #[derive(Debug, Deserialize)]
-    // pub struct Message {
-    //     pub children: Vec<Message>,
-    //     pub code: Option<String>,
-    //     pub message: String,
-    //     pub rendered: Option<String>,
-    //     pub spans: Vec<Span>,
-    // }
+fn get_clippy_output() -> Result<PathBuf, Error> {
+    let mut cmd = std::process::Command::new("cargo");
+    cmd.args(&[
+        "clippy",
+        "--target-dir",
+        &format!("{}/clippy", env!("CARGO_TARGET_DIR")),
+        "--message-format=json",
+        "--",
+        "-A",
+        "clippy::nursery",
+        "-D",
+        "clippy::style",
+        "-A",
+        "clippy::cargo",
+        "-A",
+        "clippy::pedantic",
+        "-A",
+        "clippy::restriction",
+        "-D",
+        "clippy::complexity",
+        "-D",
+        "clippy::perf",
+        "-D",
+        "clippy::correctness",
+    ]);
+    log::debug!("running {:#?}", &cmd);
 
-    // #[derive(Debug, Deserialize)]
-    // pub struct Span {
-    //     pub byte_end: usize,
-    //     pub byte_start: usize,
-    //     pub column_end: usize,
-    //     pub column_start: usize,
-    //     // TODO: double-check the type because i only found "null"s
-    //     pub expansion: Option<String>,
-    //     pub file_name: PathBuf,
-    //     pub is_primary: bool,
-    //     // TODO: double-check the type because i only found "null"s
-    //     pub label: Option<String>,
-    //     pub line_end: usize,
-    //     pub line_start: usize,
-    //     pub suggested_replacement: Option<String>,
-    //     // TODO: double-check the type because i only found "null"s
-    //     pub suggestion_applicability: Option<String>,
-    //     pub text: Vec<SpanText>,
-    // }
+    let clippy_output_raw = cmd.output()?.stdout;
+    let clippy_output_string = String::from_utf8(clippy_output_raw)?;
 
-    // #[derive(Debug, Deserialize)]
-    // pub struct SpanText {
-    //     pub highlight_end: usize,
-    //     pub highlight_start: usize,
-    //     pub text: String,
-    // }
+    // simulate `jq --slurp`
+    let sanitized = format!(
+        "[\n  {}\n] ",
+        clippy_output_string
+            .lines()
+            .collect::<Vec<_>>()
+            .join(",\n  ")
+    );
 
-    // #[derive(Debug, Deserialize)]
-    // pub struct Target {
-    //     pub kind: Vec<String>,
-    //     pub crate_types: Vec<String>,
-    //     pub name: String,
-    //     pub src_path: String,
-    //     pub edition: String,
-    //     pub doc: bool,
-    //     pub doctest: bool,
-    //     pub test: bool,
-    // }
+    // TODO: make this path configurable
+    let path = PathBuf::from("clippy.json");
 
-    // #[derive(Debug, Deserialize)]
-    // pub struct Profile {
-    //     pub opt_level: String,
-    //     pub debuginfo: Option<usize>,
-    //     pub debug_assertions: bool,
-    //     pub overflow_checks: bool,
-    //     pub test: bool,
-    // }
+    std::fs::File::create(&path)?.write_all(sanitized.as_bytes())?;
+
+    Ok(path)
 }
 
 fn main() -> Result<(), Error> {
-    let json_file = env::args().nth(1).expect("USAGE: fix-json <file or -->");
-    let json = if json_file == "--" {
-        let mut buffer = String::new();
-        BufReader::new(stdin()).read_to_string(&mut buffer)?;
-        buffer
-    } else {
-        fs::read_to_string(&json_file)?
-    };
+    env_logger::builder()
+        // .filter_level(args.log_level.to_level_filter())
+        .filter_level(log::LevelFilter::Info)
+        .format_timestamp(None)
+        .init();
 
-    // we're only interested in the compiler-message variant, so we can use a single struct.
-    // see: https://stackoverflow.com/questions/67702612/how-to-ignore-unknown-enum-variant-while-deserializing
-    use serde::Deserialize;
-    use serde_with::{serde_as, DefaultOnError};
-    #[serde_as]
-    #[derive(serde::Deserialize)]
-    pub struct W(#[serde_as(as = "Vec<DefaultOnError>")] pub Vec<Option<types::CompilerMessage>>);
+    let mut iterations = 0;
+    let mut suggestions_processed = 0;
+    let mut suggestions_processed_successfully = 0;
 
-    // Convert the JSON string to vec.
-    let clippy_lint_elements: Vec<types::CompilerMessage> = serde_json::from_str::<W>(&json)
-        .unwrap()
-        .0
-        .into_iter()
-        .flatten()
-        .collect();
+    loop {
+        iterations += 1;
+        log::info!("[{}] running clippy...", iterations);
 
-    let mut path_suggestions = HashMap::<PathBuf, Vec<rustfix::Suggestion>>::new();
+        let json_file = get_clippy_output()?;
+        let json = std::fs::read_to_string(json_file)?;
 
-    let mut num_suggestions = 0;
+        // we're only interested in the "compiler-message" reason, so we can use a single struct.
+        // see: https://stackoverflow.com/questions/67702612/how-to-ignore-unknown-enum-variant-while-deserializing
+        use serde::Deserialize;
+        use serde_with::{serde_as, DefaultOnError};
+        #[serde_as]
+        #[derive(serde::Deserialize)]
+        pub struct W(
+            #[serde_as(as = "Vec<DefaultOnError>")] pub Vec<Option<types::CompilerMessage>>,
+        );
 
-    for m in &clippy_lint_elements {
-        let mut messages = vec![&m.message];
-        while let Some(message) = messages.pop() {
-            messages.extend(message.children.iter());
-            for span in &message.spans {
-                if let Some(new_suggestion) = rustfix::collect_suggestions(
-                    message,
-                    &HashSet::new(),
-                    rustfix::Filter::Everything,
-                ) {
-                    path_suggestions
-                        .entry(PathBuf::from(span.file_name.clone()))
-                        .or_insert_with(|| vec![new_suggestion]);
-                    num_suggestions += 1;
+        // Convert the JSON string to vec.
+        let clippy_lint_elements: Vec<types::CompilerMessage> = serde_json::from_str::<W>(&json)
+            .context(format!("failed to parse output:\n{}", json))
+            .unwrap()
+            .0
+            .into_iter()
+            .flatten()
+            .collect();
+
+        let mut path_suggestions = HashMap::<PathBuf, Vec<rustfix::Suggestion>>::new();
+
+        let mut num_suggestions = 0;
+
+        for m in &clippy_lint_elements {
+            let mut messages = vec![&m.message];
+            while let Some(message) = messages.pop() {
+                messages.extend(message.children.iter());
+                for span in &message.spans {
+                    if let Some(new_suggestion) = rustfix::collect_suggestions(
+                        message,
+                        &HashSet::new(),
+                        rustfix::Filter::Everything,
+                    ) {
+                        path_suggestions
+                            .entry(PathBuf::from(span.file_name.clone()))
+                            .or_insert_with(|| vec![new_suggestion]);
+                        num_suggestions += 1;
+                    }
                 }
             }
         }
-    }
 
-    println!(
-        "got {} lints and {} files with {} suggestions",
-        clippy_lint_elements.len(),
-        path_suggestions.len(),
-        num_suggestions,
-    );
+        log::info!(
+            "[{}]: got {} lints and {} files with {} suggestions",
+            iterations,
+            clippy_lint_elements.len(),
+            path_suggestions.len(),
+            num_suggestions,
+        );
 
-    for (source_file, suggestions) in path_suggestions {
-        let source = fs::read_to_string(&source_file)?;
-        let mut fix = rustfix::CodeFix::new(&source);
-        for suggestion in suggestions.iter().rev() {
-            if let Err(e) = fix.apply(suggestion) {
-                eprintln!("Failed to apply suggestion to {:?}: {}", &source_file, e);
-            }
+        if num_suggestions == 0 {
+            log::info!("[{}] no more suggestions found, stopping", iterations);
+            break;
         }
-        let fixes = fix.finish()?;
-        fs::write(source_file, fixes)?;
+
+        let mut new_suggestions_processed = 0;
+        let mut new_suggestions_processed_successfully = 0;
+
+        for (source_file, suggestions) in path_suggestions {
+            let source = fs::read_to_string(&source_file)?;
+            let mut fix = rustfix::CodeFix::new(&source);
+            for suggestion in suggestions.iter().rev() {
+                new_suggestions_processed += 1;
+                if let Err(e) = fix.apply(suggestion) {
+                    eprintln!("Failed to apply suggestion to {:?}: {}", &source_file, e);
+                } else {
+                    new_suggestions_processed_successfully += 1;
+                }
+            }
+            let fixes = fix.finish()?;
+            fs::write(source_file, fixes)?;
+        }
+
+        println!(
+            "[{}]: suggestions processed {}, of which successful: {}",
+            iterations, new_suggestions_processed, new_suggestions_processed_successfully
+        );
+
+        suggestions_processed += new_suggestions_processed;
+        suggestions_processed_successfully += new_suggestions_processed_successfully;
     }
+
+    log::info!(
+        "{} iterations. suggestions processed {}, of which successful: {}",
+        iterations,
+        suggestions_processed,
+        suggestions_processed_successfully
+    );
 
     Ok(())
 }
